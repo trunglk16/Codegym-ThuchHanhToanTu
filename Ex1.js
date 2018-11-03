6ac3f4982c0fde439f8dbb41b12e7b5ad2bfa247
= <=>
2018-11-03 15:43:41 +0700

    ok

---
+++ Ex1.js
@@ -0,0 +1,6 @@
+var width;
+var height;
+var width = parseInt(prompt("Enter Width"));
+var height = parseInt(prompt("Enter Height"));
+var area = width * height;
+ document.write("Dien tich hinh chu nhat = " + area);
---
+++ README.md
@@ -0,0 +1,1 @@
+thuc hanh toan tu
